c25df520ff9750976dcd6eddb08d74ac60f31634
samasi25 <56556855+samasi25@users.noreply.github.com>
2025-02-25 13:21:49 +0530

    Update apiService.js

--- frontend/src/components/apiService.js
+++ frontend/src/components/apiService.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // baseURL: https://api.lyaim.com
 
 const API = axios.create({
-  baseURL: process.env.BASE_URL || "http://localhost:9000",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:9000",
   withCredentials: true,
 });
 
